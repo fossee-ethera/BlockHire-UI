6323a6c603fa71ee6b07a45c57422720bbff38ae
Akshat7321 <akshat7321.aj@gmail.com>
2019-07-09 03:33:45 +0530

    Akshat Final Change with error in minimalProfile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,11 +8,17 @@ import JobSeekerProfile from "./jobseeker/EditProfilePage";
 import CompanyProfile from "./company/EditProfilePage";
 import MainJobSeeker from "./jobseeker/MainJobSeeker";
 import MainCompany from "./company/MainCompany";
+import MinimalProfileView from "./company/MinimalProfile";
 // to not get confused with the History component in react-router.
 
 const Main = () => (
   <Router>
     <Switch>
+      <Route path="/profileview/:user_id" component={MinimalProfileView} />
+      <Route exact path="/jobs" component={MainCompany} />
+      <Route exact path="/account" component={MainCompany} />
+      <Route exact path="/validation" component={MainCompany} />
+      <Route exact path="/notifications" component={MainCompany} />
       <Route exact path="/jobseeker/profile" component={MainJobSeeker} />
       <Route exact path="/company/profile" component={MainCompany} />
       <Route exact path="/" component={HomePage} />
